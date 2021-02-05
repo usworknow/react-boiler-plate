1b09da91ba9c9b42d0f72d542a0b40325f052607
John Hull <john.o.hull@gmail.com>
2021-02-05 11:46:33 -0600

    updates

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,7 +1,30 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Landing = () => <div>Welcome to the Prep Network code challenge</div>;
+class Landing extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: ''
+		};
+	}
+	componentDidMount() {
+		this.onLoad();
+	}
+
+	onLoad() {
+		const url = `${process.env.API_URL}/get-default-message`;
+		this.setState({ loaded: false });
+		axios.get(url).then((response) => {
+			this.setState({ message: response.data });
+		});
+	}
+
+	render() {
+		return <div>{this.state.message}</div>;
+	}
+}
 
 export default Landing;
